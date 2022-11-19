a640e2bc417ca788e095e482435374baef26d55e
FranciscoVicente0 <franciscomvicente10@gmail.com>
2022-11-19 03:02:33 +0000

    Map implementation

--- app/src/main/java/com/example/gatherup/MainActivity.java
+++ app/src/main/java/com/example/gatherup/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gatherup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = new MapsActivity();
+        getSupportFragmentManager().beginTransaction().replace(R.id.Fragment, fragment).commit();
     }
 }
