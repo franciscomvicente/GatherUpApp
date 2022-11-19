100c2f921a4ccb88b15c33a139d3381f8208da0a
tomasoom <tomasgouveiaoom2@gmail.com>
2022-11-19 00:53:39 +0000

    mapazao

---
+++ app/src/main/java/com/example/gatherup/MapsActivity.java
@@ -0,0 +1,99 @@
+package com.example.gatherup;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.gatherup.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private LocationManager locationManager;
+    private Location location;
+    private double longitude;
+    private double latitude;
+    Marker marker;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 100, this);
+
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+        // Add a marker in Sydney and move the camera
+        DisplayLoc(location);
+
+    }
+
+    public void DisplayLoc(Location location){
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+        LatLng sydney = new LatLng(latitude,longitude);
+        marker = mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        marker.remove();
+        DisplayLoc(location);
+    }
+}
