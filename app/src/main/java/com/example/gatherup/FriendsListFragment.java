f8fe0fd3c05bfa8bb27414fc7439a4bf92da8764
tomasoom <tomasgouveiaoom2@gmail.com>
2022-11-23 00:07:23 +0000

    AddFriends Button
    e tentativa de trocar entre fragmentos falhada

--- app/src/main/java/com/example/gatherup/FriendsListFragment.java
+++ app/src/main/java/com/example/gatherup/FriendsListFragment.java
@@ -3,10 +3,14 @@ package com.example.gatherup;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.gatherup.databinding.FragmentAddFriendsBinding;
 
 public class FriendsListFragment extends Fragment {
 
@@ -20,4 +24,15 @@ public class FriendsListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_friends_list, container, false);
     }
+
+    public void AddFriends(View view) {
+        AddFriendsFragment addFriendsFragment = new AddFriendsFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentFriendsList, addFriendsFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
 }
