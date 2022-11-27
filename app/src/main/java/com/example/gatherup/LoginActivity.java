f15aa5de611443d77253a8d806b61ae77d30e66d
FranciscoVicente0 <franciscomvicente10@gmail.com>
2022-11-27 23:20:31 +0000

    RegisterAndLogin

--- app/src/main/java/com/example/gatherup/LoginActivity.java
+++ app/src/main/java/com/example/gatherup/LoginActivity.java
@@ -1,17 +1,82 @@
 package com.example.gatherup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"; //VERIFICAR
+    ProgressDialog progressDialog;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        progressDialog = new ProgressDialog(this);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerformLogin();
+            }
+        });
+    }
+
+    private void PerformLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("Invalid Email");
+            inputEmail.requestFocus();
+        } else if (password.isEmpty() || password.length() < 8) {
+            inputPassword.setError("Invalid Password");
+        } else {
+            progressDialog.setMessage("Please Wait...");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        AccountLogin();
+                        Toast.makeText(LoginActivity.this, "Login Completed", Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
     public void Register(View view) {
@@ -19,9 +84,10 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void Login(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+    private void AccountLogin() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
-
 }
+
--- app/src/main/java/com/example/gatherup/RegisterActivity.java
+++ app/src/main/java/com/example/gatherup/RegisterActivity.java
@@ -1,21 +1,92 @@
 package com.example.gatherup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText inputEmail, inputPassword, inputConfirmPassword;
+    Button btnRegister;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"; //VERIFICAR
+    ProgressDialog progressDialog;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressDialog= new ProgressDialog(this);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerformAuth();
+            }
+        });
     }
 
-    public void AccountCreated(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
+    private void PerformAuth(){
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmpassword = inputConfirmPassword.getText().toString();
+
+        if (!email.matches(emailPattern)) {
+            inputEmail.setError("Invalid Email");
+            inputEmail.requestFocus();
+        }else if(password.isEmpty() || password.length()<8) {
+            inputPassword.setError("Invalid Password");
+        }else if(!password.equals(confirmpassword)) {
+            inputConfirmPassword.setError("Password not match");
+        }else {
+            progressDialog.setMessage("Please Wait...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        AccountCreated();
+                        Toast.makeText(RegisterActivity.this, "Registration Completed", Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void AccountCreated() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 }
