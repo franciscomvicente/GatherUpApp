af641d6dd6108b7f4e35a5f2e628c953753e4523
AndreCachao <108025519+AndreCachao@users.noreply.github.com>
2023-01-19 04:58:49 +0000

    WhatASaveCommit

--- app/src/main/java/com/example/gatherup/ProfileFragment.java
+++ app/src/main/java/com/example/gatherup/ProfileFragment.java
@@ -100,7 +100,7 @@ public class ProfileFragment extends Fragment implements FirestoreAdapter.OnList
 
 
         friendIDs = new ArrayList<>();
-        Query queryFriends = store.collection("Users").document(profileID).collection("Friends").whereEqualTo("Status", "done");
+        Query queryFriends = store.collection("Users").document(profileID).collection("Friends").whereEqualTo("Status", "Done");
 
         queryFriends.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
